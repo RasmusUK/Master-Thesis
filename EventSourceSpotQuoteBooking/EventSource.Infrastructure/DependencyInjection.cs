da000471d4ff62e22db68739537c77d1297d8692
RasmusUK <rasmus.kristensen@live.dk>
2025-03-06 12:17:31 +0100

    Create DependencyInjection.cs

---
+++ EventSourceSpotQuoteBooking/EventSource.Infrastructure/DependencyInjection.cs
@@ -0,0 +1,36 @@
+using EventSource.Application;
+using EventSource.Application.Interfaces;
+using EventSource.Core.Interfaces;
+using EventSource.Persistence;
+using EventSource.Persistence.Interfaces;
+using EventSource.Persistence.Stores;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace EventSource.Infrastructure;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddEventSourcing(
+        this IServiceCollection services,
+        IConfiguration configuration
+    ) => services.AddApplicationServices().AddPersistence(configuration);
+
+    private static IServiceCollection AddApplicationServices(this IServiceCollection services) =>
+        services
+            .AddSingleton<IEntityHistoryService, EntityHistoryService>()
+            .AddSingleton<IPersonalDataInterceptor, PersonalDataInterceptor>()
+            .AddSingleton<IReplayService, ReplayService>()
+            .AddSingleton<IEventProcessor, EventProcessor>();
+
+    private static IServiceCollection AddPersistence(
+        this IServiceCollection services,
+        IConfiguration configuration
+    ) =>
+        services
+            .Configure<MongoDbOptions>(configuration.GetSection(MongoDbOptions.MongoDb))
+            .AddSingleton<IMongoDbService, MongoDbService>()
+            .AddSingleton<IEntityStore, MongoDbEntityStore>()
+            .AddSingleton<IEventStore, MongoDbEventStore>()
+            .AddSingleton<IPersonalDataStore, MongoDbPersonalDataStore>();
+}
