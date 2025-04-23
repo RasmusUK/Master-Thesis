e6022f453b2970d7860b114c06fee6fb60eff512
RasmusUK <rasmus.kristensen@live.dk>
2025-04-23 12:01:29 +0200

    Customers page is now working

--- EventSourceSpotQuoteBooking/SpotQuoteBooking.EventSource.Application/Interfaces/ICustomerService.cs
+++ EventSourceSpotQuoteBooking/SpotQuoteBooking.EventSource.Application/Interfaces/ICustomerService.cs
@@ -6,4 +6,11 @@ public interface ICustomerService
 {
     Task<IReadOnlyCollection<CustomerDto>> GetAllCustomersAsync();
     Task<CustomerDto?> GetCustomerByIdAsync(Guid customerId);
+    Task AddCustomerAsync(CustomerDto customer);
+
+    Task DeleteCustomerAsync(CustomerDto customer);
+
+    Task AddUserAsync(Guid customerId, UserDto user);
+
+    Task DeleteUserAsync(Guid customerId, Guid userId);
 }
