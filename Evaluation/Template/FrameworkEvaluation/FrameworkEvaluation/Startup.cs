b816d299d8bf14174912aebf903c56a16a329548
RasmusUK <rasmus.kristensen@live.dk>
2025-06-09 14:29:20 +0200

    Clean evaluation template

--- Evaluation/Template/FrameworkEvaluation/FrameworkEvaluation/Startup.cs
+++ Evaluation/Template/FrameworkEvaluation/FrameworkEvaluation/Startup.cs
@@ -1,6 +1,5 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using EventSourcingFramework.Infrastructure.DI;
 
 namespace FrameworkEvaluation;
 
@@ -8,17 +7,5 @@ public class Startup
 {
     public void ConfigureServices(IServiceCollection services)
     {
-        var configuration = new ConfigurationBuilder()
-          .SetBasePath(Directory.GetCurrentDirectory())
-          .AddJsonFile("appsettings.json", false, true)
-          .Build();
-
-        services.AddEventSourcing(configuration, (schema, migrations, migrator, mongoDbRegistrationService) =>
-        {
-            mongoDbRegistrationService.Register((typeof(SpotQuote), "Spotquotes"), (typeof(Customer), "Customers"));
-
-            // Register entities
-            // Register migrations
-        });
     }
 }
