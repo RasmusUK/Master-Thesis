e34a5834c13e55c837920f10b8f0cc4f7609546a
RasmusUK <rasmus.kristensen@live.dk>
2025-06-21 13:47:46 +0200

    Add performance

--- EventSourcingFramework/tests/Performance/EventSourcingFramework.Test.Performance/RepositoryPerformanceTests.cs
+++ EventSourcingFramework/tests/Performance/EventSourcingFramework.Test.Performance/RepositoryPerformanceTests.cs
@@ -1,4 +1,5 @@
 ﻿using System.Diagnostics;
+using EventSourcingFramework.Application.Abstractions.Replay;
 using EventSourcingFramework.Application.Abstractions.ReplayContext;
 using EventSourcingFramework.Core.Interfaces;
 using EventSourcingFramework.Infrastructure.Shared.Interfaces;
@@ -58,5 +59,60 @@ public class RepositoryPerformanceTests : MongoIntegrationTestBase
         }
 
         testOutputHelper.WriteLine($"Average duration: {durations.Average()} ms");
+    }
+
+    [Trait("Category", "Performance")]
+    [Fact]
+    public async Task ReadPerformance()
+    {
+        var provider = ServiceProvider.BuildServiceProviderWithSettings();
+        var repo = provider.GetRequiredService<IRepository<TestEntity2>>();
+        var eventStore = provider.GetRequiredService<IEventStore>();
+        var replayService = provider.GetRequiredService<IReplayService>();
+        
+        var entity = TestEntityFactory.CreateTestEntity2();
+        await repo.CreateAsync(entity);
+     
+        for (var j = 0; j < 9; j++)
+        {
+            await repo.UpdateAsync(entity);
+        }
+     
+        var sw = Stopwatch.StartNew();
+        _ = await repo.ReadByIdAsync(entity.Id);
+        sw.Stop();
+        testOutputHelper.WriteLine($"Read full entity took: {sw.ElapsedMilliseconds} ms");
+        
+        sw = Stopwatch.StartNew();
+        _ = await repo.ReadProjectionByFilterAsync(
+            e => e.Id == entity.Id,
+            e => e.Name
+        );
+        sw.Stop();
+        testOutputHelper.WriteLine(
+            $"Read projection by filter took: {sw.ElapsedMilliseconds} ms"
+        );
+     
+        sw = Stopwatch.StartNew();
+        var events = await eventStore.GetEventsByEntityIdAsync(entity.Id);
+        await replayService.ReplayEventsAsync(events);
+        _ = await repo.ReadByIdAsync(entity.Id);
+        sw.Stop();
+        testOutputHelper.WriteLine(
+            $"Read entity by events took: {sw.ElapsedMilliseconds} ms"
+        );
+        
+     
+        sw = Stopwatch.StartNew();
+        events = await eventStore.GetEventsByEntityIdAsync(entity.Id);
+        await replayService.ReplayEventsAsync(events);
+        _ = await repo.ReadProjectionByFilterAsync(
+            e => e.Id == entity.Id,
+            e => e.Name
+        );
+        sw.Stop();
+        testOutputHelper.WriteLine(
+            $"Read projection by events took: {sw.ElapsedMilliseconds} ms"
+        );
     }
 }
--- EventSourcingFramework/tests/Performance/EventSourcingFramework.Test.Performance/TestEntityFactory.cs
+++ EventSourcingFramework/tests/Performance/EventSourcingFramework.Test.Performance/TestEntityFactory.cs
@@ -27,4 +27,43 @@ public static class TestEntityFactory
             Nr10 = 1000000000
         };
     }
+
+    public static TestEntity2 CreateTestEntity2(int childrenCount = 3)
+    {
+        return new TestEntity2
+        {
+            Id = Guid.NewGuid(),
+            ConcurrencyVersion = 1,
+            SchemaVersion = 1,
+            Name = string.Empty,
+            Name1 = "Name",
+            Name2 = "NameName",
+            Name3 = "NameNameName",
+            Name4 = "NameNameNameName",
+            Name5 = "NameNameNameNameName",
+            Nr1 = 1,
+            Nr2 = 10,
+            Nr3 = 100,
+            Nr4 = 1000,
+            Nr5 = 10000,
+            Nr6 = 100000,
+            Nr7 = 1000000,
+            Nr8 = 10000000,
+            Nr9 = 100000000,
+            Nr10 = 1000000000,
+            Children = childrenCount == 0 ? new List<TestEntity2>() : new List<TestEntity2>
+            {
+                CreateTestEntity2(childrenCount-1),
+                CreateTestEntity2(childrenCount-1),
+                CreateTestEntity2(childrenCount-1),
+                CreateTestEntity2(childrenCount-1),
+                CreateTestEntity2(childrenCount-1),
+                CreateTestEntity2(childrenCount-1),
+                CreateTestEntity2(childrenCount-1),
+                CreateTestEntity2(childrenCount-1),
+                CreateTestEntity2(childrenCount-1),
+                CreateTestEntity2(childrenCount-1)
+            }
+        };
+    }
 }
